f9dfef54b2657b98f1cb867ec750bced5ff8a86d
Michael Walker <walkerrepair255@yahoo.com>
2021-09-30 12:12:45 -0400

    notes

--- BarkingDogsInterfacePractise/Program.cs
+++ BarkingDogsInterfacePractise/Program.cs
@@ -16,11 +16,12 @@ namespace BarkingDogsInterfacePractise
             foreach (var dog in dogs)        // dog is created in this foreach
             {
                 Console.WriteLine($"{dog.Name} goes... {dog.Bark()}");
-                var chawawa = dog as Chuhuahua;
+                var chawawa = dog as Chuhuahua;    //this allows you to us the var chawawa to call the Walk() method from Chuhuahua
                 if (chawawa != null)
                 {
                     Console.WriteLine(chawawa.Walk());
                 }
+                
             }
         }
     }
